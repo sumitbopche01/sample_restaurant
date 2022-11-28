f996c5ec23c14050c2dde31e7bca87a927132606
Sumit Bopche <sumitbopche01@gmail.com>
2022-11-28 18:47:01 -0500

    modified schema's as per the eslint standard

--- src/utils/helper.util.js
+++ src/utils/helper.util.js
@@ -1,7 +1,8 @@
+// eslint-disable-next-line default-param-last
 function getOffset(currentPage = 1, listPerPage) {
-    return (currentPage - 1) * [listPerPage];
-  }
-  
+  return (currentPage - 1) * [listPerPage];
+}
+
 function emptyOrRows(rows) {
   if (!rows) {
     return [];
@@ -11,5 +12,5 @@ function emptyOrRows(rows) {
 
 module.exports = {
   getOffset,
-  emptyOrRows
-}
+  emptyOrRows,
+};
