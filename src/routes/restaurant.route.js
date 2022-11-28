3d8c56cba99d78c741cd61efad66c16f6c076c0c
Sumit Bopche <sumitbopche01@gmail.com>
2022-11-28 02:30:31 -0500

    restaurant route

---
+++ src/routes/restaurant.route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const restaurantController = require('../controller/restaurant.controller');
+
+/* GET restaurant  */
+router.get('/', restaurantController.get);
+  
+/* POST create restaurant */
+router.post('/', restaurantController.create);
+
+/* PUT restaurants */
+router.put('/:id', restaurantController.update);
+
+/* DELETE restaurants */
+router.delete('/:id', restaurantController.remove);
+
+module.exports = router;
