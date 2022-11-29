5a3641ededb1072830eeb9f6aca27ff2cde5dc7e
Sumit Bopche <sumitbopche01@gmail.com>
2022-11-29 15:04:39 -0500

    added use signup and login routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,45 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable comma-dangle */
 const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
-const userController = require('../controller/users.controller');
 
-/* GET restaurant  */
-router.post('/signup', userController.signUp);
+router.post(
+  '/signup',
+  passport.authenticate('signup', { session: false }),
+  async (req, res) => {
+    res.json({
+      message: 'Signup successful',
+      user: req.user,
+    });
+  }
+);
 
-router.post('/login', userController.login);
+router.post('/login', async (req, res, next) => {
+  // eslint-disable-next-line no-unused-vars
+  passport.authenticate('login', async (err, user, info) => {
+    try {
+      if (err || !user) {
+        const error = new Error('An error occurred.');
+
+        return next(error);
+      }
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+
+        const body = { _id: user._id, email: user.email };
+        const token = jwt.sign({ user: body }, 'TOP_SECRET');
+
+        return res.json({ token });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
 
 module.exports = router;
