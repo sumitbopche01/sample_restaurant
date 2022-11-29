c3ee49c2254bd414422aa9b41d41442e1b8f29e4
Sumit Bopche <sumitbopche01@gmail.com>
2022-11-29 10:19:31 -0500

    user controller, model and routes

---
+++ src/models/users.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  username: { type: String, default: '' },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+module.exports = mongoose.model('users', UserSchema);
